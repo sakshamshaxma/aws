6292debf58b521edc9351d507ee479e5f75115f9
Saksham Sharma <161831957+sakshamshaxma@users.noreply.github.com>
2025-01-23 12:56:55 +0530

    Update main.tf

--- modules/elasticache/main.tf
+++ modules/elasticache/main.tf
@@ -1,15 +1,15 @@
 resource "aws_elasticache_subnet_group" "cache" {
   name       = "cache-subnet-group"
-  subnet_ids = var.public_subnet_ids
+  subnet_ids = var.private_subnet_ids
   tags       = var.tags
 }
 
 resource "aws_elasticache_cluster" "cache" {
   cluster_id           = "cache-cluster"
-  engine               = "valkey"
+  engine               = "redis" # Corrected engine from "valkey" to "redis"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.valkey3.2"
+  parameter_group_name = "default.redis3.2"
   subnet_group_name    = aws_elasticache_subnet_group.cache.name
   tags                 = var.tags
 }
