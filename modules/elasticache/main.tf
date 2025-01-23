2c94a2bc98df51dad28651435ea5577ad042b50a
Saksham Sharma <161831957+sakshamshaxma@users.noreply.github.com>
2025-01-23 13:12:48 +0530

    Update main.tf

--- modules/elasticache/main.tf
+++ modules/elasticache/main.tf
@@ -5,11 +5,10 @@ resource "aws_elasticache_subnet_group" "cache" {
 }
 
 resource "aws_elasticache_cluster" "cache" {
-  cluster_id           = "cache-cluster"
-  engine               = "redis" # Corrected engine from "valkey" to "redis"
-  node_type            = "cache.t2.micro"
-  num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
-  subnet_group_name    = aws_elasticache_subnet_group.cache.name
-  tags                 = var.tags
+  engine       = "redis"
+  engine_version = "7.0"  # Make sure to use the correct version for Redis 7
+  cluster_id   = "cache-cluster"
+  node_type    = "cache.t2.micro"
+  num_cache_nodes = 1
+  # Other configurations as required
 }
