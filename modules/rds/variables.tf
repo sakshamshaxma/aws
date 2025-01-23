54e989f22d6594adc8feb02773d4b8e1f4bf5245
Saksham Sharma <161831957+sakshamshaxma@users.noreply.github.com>
2025-01-23 12:58:19 +0530

    Update variables.tf

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -1,9 +1,19 @@
-variable "public_subnet_ids" {
+variable "rds_username" {
+  type        = string
+  description = "The username for the RDS instance."
+}
+
+variable "rds_password" {
+  type        = string
+  description = "The password for the RDS instance."
+}
+
+variable "private_subnet_ids" {
   type        = list(string)
-  description = "The IDs of the public subnets."
+  description = "The IDs of the private subnets."
 }
 
 variable "tags" {
   type        = map(string)
-  description = "Tags for the resources."
+  description = "Tags for the RDS resources."
 }
