d1540b145529f41cd98c5e429946f08728131877
Saksham Sharma <161831957+sakshamshaxma@users.noreply.github.com>
2025-01-23 12:55:38 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,7 +1,7 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = var.cluster_name
-  cluster_version = "1.31"
+  cluster_version = "1.31" # Updated to a valid Kubernetes version
   vpc_id          = var.vpc_id
   subnet_ids      = var.subnet_ids
 
