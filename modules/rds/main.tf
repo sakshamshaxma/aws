5e700a5ccb79af460ad74d192ab211b0f12d911a
Saksham Sharma <161831957+sakshamshaxma@users.noreply.github.com>
2025-01-23 13:13:28 +0530

    Update main.tf

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -7,12 +7,12 @@ resource "aws_db_subnet_group" "rds" {
 resource "aws_db_instance" "rds" {
   allocated_storage    = 5
   engine               = "mysql"
-  engine_version       = "8.0"
-  instance_class       = "db.t2.micro"
+  engine_version       = "5.6"
+  instance_class       = "db.t3.micro"
   identifier           = "saksham"
   username             = var.rds_username  # Use the username variable
   password             = var.rds_password  # Use the password variable
-  parameter_group_name = "default.mysql8.0"
+  parameter_group_name = "default.mysql5.6"
   publicly_accessible  = true
   db_subnet_group_name = aws_db_subnet_group.rds.name
   skip_final_snapshot  = true
